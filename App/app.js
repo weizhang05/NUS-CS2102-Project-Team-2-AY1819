cd6a4dba70b15349660e4306a0f5f6ee6e47bda4
weizhang05 <chanweizhang05@hotmail.com>
2019-02-13 01:09:32 +0800

    added skeleton files updated README.md

--- App/app.js
+++ App/app.js
@@ -7,6 +7,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var loginRouter = require('./routes/login');
+var registerRouter = require('./routes/register');
+var reservationRouter = require('./routes/reservation');
+var aboutRouter = require('./routes/about');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +26,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
+app.use('/reservation', reservationRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CS2102-Team-2
 NUS CS2102 Team 2 Project
+
+Setting up commands:
+```
+cd App
+npm install
+```
