271da1aa524de524535dd8dd5303792291d8ef4b
weizhang05 <chanweizhang05@hotmail.com>
2019-03-03 01:55:43 +0800

    added and created login and register (half complete) functionality

--- App/routes/register.js
+++ App/routes/register.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+const { Pool } = require('pg')
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'postgres',
+  port: 5432,
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('register', { title: 'Register' });
+});
+
+// POST
+router.post('/', function(req, res, next) {
+	var name = req.body.name;
+	var email = req.body.email;
+	var pw = req.body.password;
+	var confirmPw = req.body.confirmPassword;
+	
+	var registerQuery = "SELECT * FROM customer WHERE '"+email+"' = email";
+	
+	pool.query(registerQuery, (err, data) => {
+		if(data["rowCount"] == 1){
+			console.log("User exists!");
+		}
+		else{
+			 console.log("meowwww");
+		}
+		
+		res.redirect('/register')
+	});
 });
 
 module.exports = router;
