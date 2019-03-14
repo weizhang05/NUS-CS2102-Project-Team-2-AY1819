8d7408c43d53d2a9a1d6db32d36dd415ddb23c53
Johannes Choo <jhanschoo@gmail.com>
2019-03-14 13:35:02 +0800

    refactor hardcoded db connection

--- App/routes/reservation.js
+++ App/routes/reservation.js
@@ -1,14 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const { Pool } = require('pg')
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'postgres',
-  port: 5432,
-})
+const pool = require('../pool');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
