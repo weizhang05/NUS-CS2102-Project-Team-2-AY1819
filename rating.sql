ccb4a2979425b813876448968493cdda8b0f395a
pratyayj <pratyay.jaidev@gmail.com>
2019-04-11 02:53:47 +0800

    Add rating.sql file for rating table

--- rating.sql
+++ rating.sql
@@ -4,6 +4,6 @@ CREATE TABLE rating (
   id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   customer_id uuid NOT NULL REFERENCES customer ON DELETE CASCADE,
   branch_id uuid NOT NULL REFERENCES branch ON DELETE CASCADE,
-  rating integer NOT NULL CONSTRAINT rating_limits CHECK (rating >= 1 AND rating <= 5),
+  rating_value integer NOT NULL CONSTRAINT rating_limits CHECK (rating_value >= 1 AND rating_value <= 5),
   UNIQUE (customer_id, branch_id)
 );
