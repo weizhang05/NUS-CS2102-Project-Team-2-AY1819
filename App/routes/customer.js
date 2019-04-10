2c3a77b1fe99d3181e2cd25ceff66c057124d907
Alex Fong <alexfongg@gmail.com>
2019-04-10 13:00:56 +0800

    Fixed basic customer reservation (without operating override, without error msgs)

--- App/routes/customer.js
+++ App/routes/customer.js
@@ -79,6 +79,7 @@ router.post('/login', function(req, res, next) {
 
 // List reservation
 router.get('/reservation', function(req, res, next) {
+	console.log(req.cookies);
 	let customerCookie = req.cookies.customer[0];
 	var getCuisineQuery = "SELECT * FROM booking WHERE customer_id = '"+customerCookie["id"]+"'";
 	pool.query(getCuisineQuery, (err, data) => {
@@ -113,10 +114,10 @@ router.post('/selectRestaurant', function(req, res, next) {
 // Select branch
 
 const RESTAURANT_BRANCHES_QUERY = `
-SELECT * 
-FROM branch b join restaurant r 
+SELECT b.id id, restaurant_name, b.name, address, plus_code 
+FROM branch b join restaurant r
 ON b.restaurant_id = r.id
-WHERE restaurant_id = $1
+WHERE b.restaurant_id = $1
 `;
 
 router.get('/selectBranch', function(req, res, next) {
@@ -125,55 +126,82 @@ router.get('/selectBranch', function(req, res, next) {
 router.post('/selectBranch', function(req, res, next) {
 	const restaurant_id = req.body.restaurant;
 	pool.query(RESTAURANT_BRANCHES_QUERY, [restaurant_id], (err, branchesData) => {
-		rname = branchesData.rows[0].restaurant_name;
-		res.render('selectBranch', { title: 'Select Branch', restaurant_name: rname, data: branchesData.rows });
+	  if (err) {
+	  	console.log(err);
+		} else {
+      rname = branchesData.rows[0].restaurant_name;
+      res.render('selectBranch', { title: 'Select Branch', restaurant_name: rname, data: branchesData.rows });
+		}
 	});
 });
 
 
 // 1) check if datetime in opening hours
+// 		4 cases:
+//		 	1) booking_start is on start_day
+//		 	booking_end is before end_day
+//		 	2) booking_start is after start day
+//		 	booking_end is on end_day
+//		 	3) booking_start is on start_day
+//		 	booking_end is on end_day
+//		 	4) book_start is after start_day
+//		 	book_end is before end_day
 // 2) check if branch has capacity
 // 3) check operating hours override
 
 // $1 = branch id
 // $2 = start_day (int 0-6)
 // $3 = start_time (time)
-// $4 = start_ts (timestamp)
-// $5 = end_day (int 0-6)
-// $6 = end_time (time)
+// $4 = end_day
+// $5 = end_time (int 0-6)
+// $6 = start_ts (time)
 // $7 = end_ts (timestamp)
 // $8 = booking_pax
 
 // TODO: add operating hrs override after basic version works
 // assumes that opening_hours for the branch has no strange overlaps
 const CHECK_AVAILABILITY_QUERY = `
-SELECT 1 
+SELECT 1
 FROM opening_hours op1
-WHERE op1.branch_id = $1 and 
+WHERE op1.branch_id = $1 and
 EXISTS (
-	select 1 
-	FROM opening_hours op2
-	WHERE op2.branch_id = op1.branch_id, 
-				op2.start_day <= $2,
-				op2.start_time <= $3,
-				op2.end_day >= $5,
-				op2.end_time > $6
-) and 
+  select 1
+  FROM opening_hours op2
+  WHERE op2.branch_id = op1.branch_id
+  AND (
+  (op2.start_day = $2
+  AND op2.start_time <= $3
+  AND op2.end_day > $4)
+  OR
+  (op2.start_day < $2
+  AND op2.end_time >= $5
+  AND op2.end_day = $4)
+  OR
+  (op2.start_day = $2
+  AND op2.end_day = $4
+  AND op2.start_time <= $3 
+  AND op2.end_time >= $5)
+  OR
+  (op2.start_day < $2
+  AND op2.end_day > $4)
+	)
+) AND 
 EXISTS (
 	select 1
 	FROM branch br
-	WHERE br.branch_id = op1.branch_id,
-				(SELECT SUM(pax) 
-					FROM booking bk
-          WHERE br.branch_id = bk.branch_id
-          	AND b.throughout <@ tsrange($4, $7, '[)') <= br.capcacity - $8
-
+	WHERE br.id = op1.branch_id
+	AND 
+      br.capacity - $8 >= 
+      (SELECT COALESCE(SUM(bk.pax), 0)
+        FROM booking bk
+        WHERE br.id = bk.branch_id
+        AND bk.throughout && tsrange($6, $7, '[)'))
 )
 `;
 
-// save to booking
 const MAKE_BOOKING_QUERY = `
-INSERT INTO booking(customer_id, branch_id, pax, throughout) VALUES($1, $2, $3, $4)
+INSERT INTO booking(customer_id, branch_id, pax, throughout)
+VALUES($1, $2, $3, tsrange($4, $5, '[)'))
 `;
 
 // Reservation (End)
@@ -182,16 +210,53 @@ router.get('/makeReservation', function(req, res, next) {
 });
 
 router.post('/makeReservation', function(req, res, next) {
-	console.log("making reservation: ");
-	console.log(req.body);
-	const {branch_id, reservation_pax, reservation_datetime, duration} = req.body;
-	
-	pool.query(CHECK_AVAILABILITY_QUERY, [branch_id, reservation_pax, reservation_datetime, duration], (err, data) => {
-		console.log(data);
-		if(data.rows[0]){
-			pool.query(MAKE_BOOKING_QUERY, [branch_id, reservation_pax, reservation_datetime, duration], (err, data) => {
-			});
-			res.render('makeReservation', { title: 'Booking is done!', data: data.rows });
+	const {branch_id, reservation_pax, reservation_datetime, duration_mins} = req.body;
+
+	const start_time = new Date(reservation_datetime);
+  const end_time = new Date(start_time.getTime() + duration_mins * 60 * 1000);
+
+	function get_seconds_in_day(date) {
+    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+	}
+
+  const availability_data = [branch_id,
+  	start_time.getDay(),
+  	get_seconds_in_day(start_time),
+  	end_time.getDay(),
+    get_seconds_in_day(end_time),
+		start_time,
+		end_time,
+		reservation_pax
+	];
+
+  console.log(availability_data);
+
+	pool.query(CHECK_AVAILABILITY_QUERY, availability_data, (err, data) => {
+	  if (err) {
+      console.log("error with avail query");
+      console.log(err);
+		} else {
+
+      console.log(data);
+	  	const hasAvailability = data.rowCount === 1;
+
+      if(hasAvailability){
+
+      	const customerId = req.cookies.customer[0].id;
+        const make_booking_data = [customerId, branch_id, reservation_pax, start_time, end_time];
+        console.log(make_booking_data);
+
+        pool.query(MAKE_BOOKING_QUERY, make_booking_data, (err, data) => {
+          if (err) {
+            console.log("error with making booking");
+            console.log(err);
+          } else {
+            res.render('makeReservation', { title: 'Booking is done!', data: data.rows });
+					}
+        });
+      } else {
+        console.log("no availablity");
+			}
 		}
 	});
 });
--- mock_data_generation.sql
+++ mock_data_generation.sql
@@ -4,14 +4,16 @@ INSERT INTO cuisine(name) values('italian');
 
 -- INSERT RESTAURANT AND BRANCH
 WITH rows AS (
-INSERT INTO restaurant(account_name, restaurant_name) VALUES('pastamania', 'Pasta Mania')
+INSERT INTO restaurant(account_name, restaurant_name)
+VALUES('pastamania', 'Pasta Mania')
 RETURNING id, 'pasta_branch1', 'def street 123', 100
 )
 INSERT INTO branch(restaurant_id, name, address, capacity)
 SELECT * FROM rows;
 
 WITH rows AS (
-INSERT INTO restaurant(account_name, restaurant_name) VALUES('whiterabbit', 'White Rabbit')
+INSERT INTO restaurant(account_name, restaurant_name)
+VALUES('whiterabbit', 'White Rabbit')
 RETURNING id, 'rabbit_branch1', 'abc street 123', 100
 )
 INSERT INTO branch(restaurant_id, name, address, capacity)
@@ -19,19 +21,55 @@ SELECT * FROM rows;
 
 -- INSERT RESTAURANT_CUISINE
 WITH rows AS (
-SELECT r.id as rid, c.id as cid FROM restaurant r, cuisine c WHERE r.restaurant_name = 'Pasta Mania' AND c.name = 'italian'
+SELECT r.id as rid, c.id as cid
+FROM restaurant r, cuisine c
+WHERE r.restaurant_name = 'Pasta Mania' AND c.name = 'italian'
 )
 INSERT INTO restaurant_cuisine(restaurant_id, cuisine_id)
 SELECT * FROM rows;
 
 WITH rows AS (
-SELECT r.id as rid, c.id as cid FROM restaurant r, cuisine c WHERE r.restaurant_name = 'White Rabbit' AND c.name = 'european'
+SELECT r.id as rid, c.id as cid
+FROM restaurant r, cuisine c
+WHERE r.restaurant_name = 'White Rabbit' AND c.name = 'european'
 )
 INSERT INTO restaurant_cuisine(restaurant_id, cuisine_id)
 SELECT * FROM rows;
 
 -- INSERT CUSTOMER
-INSERT INTO customer(name, email, password, non_user) VALUES('clyde','clyde@email.com','clyde',false);
+INSERT INTO customer(name, email, password, non_user)
+VALUES('clyde','clyde@email.com','clyde',false),
+('a','a@a.a','a',false)
+;
 
 -- INSERT ADMIN
-INSERT INTO admins (account_name) VALUES ('admin1'), ('admin2');
+INSERT INTO admins (account_name)
+VALUES ('admin1'), ('admin2');
+
+-- INSERT BOOKING HRS
+WITH rest AS (
+SELECT b.id, 0, '22:00'::time, 1, '02:00'::time
+FROM branch b
+WHERE b.name = 'rabbit_branch1'
+)
+INSERT INTO opening_hours(branch_id, start_day, start_time, end_day, end_time)
+SELECT * FROM rest;
+
+-- INSERT BOOKING
+WITH item AS (
+SELECT c.id, b.id, 1, tsrange('2019-04-14T16:00:00.000Z', '2019-04-14T17:00:00.000Z','[)')
+FROM customer c, branch b
+WHERE b.name = 'rabbit_branch1' and c.name = 'a'
+)
+INSERT INTO booking(customer_id, branch_id, pax, throughout)
+SELECT * FROM item;
+
+WITH item AS (
+SELECT c.id, b.id, 1, tsrange('2019-04-14T16:00:00.000Z', '2019-04-14T17:30:00.000Z','[)')
+FROM customer c, branch b
+WHERE b.name = 'rabbit_branch1' and c.name = 'clyde'
+)
+INSERT INTO booking(customer_id, branch_id, pax, throughout)
+SELECT * FROM item;
+
+-- tables below not used for milestone 1
