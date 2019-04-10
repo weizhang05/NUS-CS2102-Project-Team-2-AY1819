3d2fac1f89aa602c5eb8c17ccc58b7e08cdc6e71
weizhang05 <chanweizhang05@hotmail.com>
2019-04-10 17:24:23 +0800

    updated mock data

--- mock_data_generation.sql
+++ mock_data_generation.sql
@@ -1,6 +1,7 @@
 -- INSERT CUISINE
 INSERT INTO cuisine(name) values('European');
 INSERT INTO cuisine(name) values('Italian');
+INSERT INTO cuisine(name) values('Thai');
 
 -- INSERT RESTAURANT AND BRANCH
 WITH rows AS (
@@ -46,7 +47,7 @@ SELECT * FROM rows;
 WITH rows AS (
 SELECT r.id as rid, c.id as cid
 FROM restaurant r, cuisine c
-WHERE r.restaurant_name = 'Pasta Mania' AND c.name = 'italian'
+WHERE r.restaurant_name = 'Pasta Mania' AND c.name = 'Italian'
 )
 INSERT INTO restaurant_cuisine(restaurant_id, cuisine_id)
 SELECT * FROM rows;
@@ -54,7 +55,15 @@ SELECT * FROM rows;
 WITH rows AS (
 SELECT r.id as rid, c.id as cid
 FROM restaurant r, cuisine c
-WHERE r.restaurant_name = 'White Rabbit' AND c.name = 'european'
+WHERE r.restaurant_name = 'White Rabbit' AND c.name = 'European'
+)
+INSERT INTO restaurant_cuisine(restaurant_id, cuisine_id)
+SELECT * FROM rows;
+
+WITH rows AS (
+SELECT r.id as rid, c.id as cid
+FROM restaurant r, cuisine c
+WHERE r.restaurant_name = 'Bangkok Jam' AND c.name = 'Thai'
 )
 INSERT INTO restaurant_cuisine(restaurant_id, cuisine_id)
 SELECT * FROM rows;
@@ -62,8 +71,7 @@ SELECT * FROM rows;
 -- INSERT CUSTOMER
 INSERT INTO customer(name, email, password, non_user)
 VALUES('clyde','clyde@email.com','clyde',false),
-('a','a@a.a','a',false)
-;
+('a','a@a.a','a',false);
 
 -- INSERT ADMIN
 INSERT INTO admins (account_name)
@@ -73,7 +81,7 @@ VALUES ('admin1'), ('admin2');
 WITH rest AS (
 SELECT b.id, 0, '22:00'::time, 1, '02:00'::time
 FROM branch b
-WHERE b.name = 'rabbit_branch1'
+WHERE b.name = 'W'
 )
 INSERT INTO opening_hours(branch_id, start_day, start_time, end_day, end_time)
 SELECT * FROM rest;
@@ -82,7 +90,7 @@ SELECT * FROM rest;
 WITH item AS (
 SELECT c.id, b.id, 1, tsrange('2019-04-14T16:00:00.000Z', '2019-04-14T17:00:00.000Z','[)')
 FROM customer c, branch b
-WHERE b.name = 'rabbit_branch1' and c.name = 'a'
+WHERE b.name = 'BKJ @ GWC' and c.name = 'a'
 )
 INSERT INTO booking(customer_id, branch_id, pax, throughout)
 SELECT * FROM item;
@@ -90,7 +98,7 @@ SELECT * FROM item;
 WITH item AS (
 SELECT c.id, b.id, 1, tsrange('2019-04-14T16:00:00.000Z', '2019-04-14T17:30:00.000Z','[)')
 FROM customer c, branch b
-WHERE b.name = 'rabbit_branch1' and c.name = 'clyde'
+WHERE b.name = 'White Rabbit @ Harding' and c.name = 'clyde'
 )
 INSERT INTO booking(customer_id, branch_id, pax, throughout)
 SELECT * FROM item;
