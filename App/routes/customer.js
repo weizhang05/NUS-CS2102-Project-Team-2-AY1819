ba63ed5f09fe165101ffccbb1788faaccd80dd62
Johannes Choo <jhanschoo@gmail.com>
2019-04-11 14:07:44 +0800

    fix make booking query

--- App/routes/customer.js
+++ App/routes/customer.js
@@ -16,56 +16,9 @@ let router = express.Router();
 // 2) check if branch has capacity
 // 3) check operating hours override
 
-// $1 = branch id
-// $2 = start_day (int 0-6)
-// $3 = start_time (time)
-// $4 = end_day
-// $5 = end_time
-// $6 = start_ts (timestamp)
-// $7 = end_ts
-// $8 = booking_pax
-const CHECK_BRANCH_AVAILABILITY_QUERY = `
-SELECT 1
-FROM opening_hours op1
-WHERE op1.branch_id = $1 and
-EXISTS (
-  select 1
-  FROM opening_hours op2
-  WHERE op2.branch_id = op1.branch_id
-  AND (
-  (op2.start_day = $2
-  AND op2.start_time <= $3
-  AND op2.end_day > $4)
-  OR
-  (op2.start_day < $2
-  AND op2.end_time >= $5
-  AND op2.end_day = $4)
-  OR
-  (op2.start_day = $2
-  AND op2.end_day = $4
-  AND op2.start_time <= $3 
-  AND op2.end_time >= $5)
-  OR
-  (op2.start_day < $2
-  AND op2.end_day > $4)
-	)
-) AND 
-EXISTS (
-	select 1
-	FROM branch br
-	WHERE br.id = op1.branch_id
-	AND 
-      br.capacity - $8 >= 
-      (SELECT COALESCE(SUM(bk.pax), 0)
-        FROM booking bk
-        WHERE br.id = bk.branch_id
-        AND bk.throughout && tsrange($6, $7, '[)'))
-)
-`;
-
 const MAKE_BOOKING_QUERY = `
 INSERT INTO booking(customer_id, branch_id, pax, throughout)
-VALUES($1, $2, $3, tsrange($4, $5, '[)'))
+VALUES($1, $2, $3, $4)
 `;
 
 const GET_RESTAURANT_BRANCHES_QUERY = `
@@ -285,72 +238,21 @@ router.get('/customer/makeReservation', function(req, res, next) {
 
 router.post('/customer/makeReservation', function(req, res, next) {
 	console.log(req.body);
-	const {branch_id, reservation_pax, reservation_datetime, duration_mins} = req.body;
+	const {branch_id, reservation_pax, reservation_start, reservation_end} = req.body;
+	console.log("ATTEMPTING TO INSERT");
+		const customerId = req.cookies.customer[0].id;
+		const make_booking_data = [customerId, branch_id, reservation_pax, `[${reservation_start}, ${reservation_end}]`];
+		console.log(make_booking_data);
 
-	const start_time = new Date(reservation_datetime);
-  const end_time = new Date(start_time.getTime() + duration_mins * 60 * 1000);
-
-	function get_seconds_in_day(date) {
-    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-	}
-
-  const availability_data = [branch_id,
-  	start_time.getDay(),
-  	get_seconds_in_day(start_time),
-  	end_time.getDay(),
-    get_seconds_in_day(end_time),
-		start_time,
-		end_time,
-		reservation_pax
-	];
-	
-	pool.query(CHECK_BRANCH_AVAILABILITY_QUERY, availability_data, (err, data) => {
-	  if (err) {
-      console.log("error with avail query");
-      console.log(err);
-		} else {
-	  	const hasAvailability = data.rowCount === 1;
-		console.log(data);
-      if(hasAvailability){
-		  console.log("ATTEMPTING TO INSERT");
-      	const customerId = req.cookies.customer[0].id;
-        const make_booking_data = [customerId, branch_id, reservation_pax, start_time, end_time];
-        console.log(make_booking_data);
-
-        pool.query(MAKE_BOOKING_QUERY, make_booking_data, (err, data) => {
-          if (err) {
-            console.log("error with making booking");
-            console.log(err);
-          } else {
-			  console.log("SUCCESS");
-            res.render('makeReservation', { title: 'Booking is done!', data: data.rows });
-					}
-        });
-      } 
-		else {
-			console.log("no availability");
-			const getRestaurantId = "SELECT DISTINCT restaurant_id AS id FROM branch WHERE id = '"+branch_id+"'"
-			pool.query(getRestaurantId, (err, branchesData) => {
-				if (err) {
-					console.log(err);
-				}
-				else {
-					rId = branchesData.rows[0].id;
-
-					pool.query(GET_RESTAURANT_BRANCHES_QUERY, [rId], (err, branchesData) => {
-						if (err) {
-							console.log(err);
-						} 
-						else {
-							rname = branchesData.rows[0].restaurant_name;
-							res.render('selectBranch', { title: 'Select Branch', restaurant_name: rname, data: branchesData.rows });
-						}
-					});
-				}
-			});
-		}
-		}
-	});
+		pool.query(MAKE_BOOKING_QUERY, make_booking_data, (err, data) => {
+			if (err) {
+				console.log("error with making booking");
+				console.log(err);
+			} else {
+		console.log("SUCCESS");
+				res.render('makeReservation', { title: 'Booking is done!', data: data.rows });
+			}
+		});
 });
 
 // Display view/submit rating
